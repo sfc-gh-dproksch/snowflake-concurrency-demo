bd168ceea63908e7dbd4a0e68b9c3346e213163b
David Proksch <david.proksch@gmail.com>
2019-03-08 16:50:16 -0500

    Updating comments

--- backend/src/main/java/com/snowflake/demo/concurrency/backend/SnowflakeConnection.java
+++ backend/src/main/java/com/snowflake/demo/concurrency/backend/SnowflakeConnection.java
@@ -10,12 +10,25 @@ import net.snowflake.client.jdbc.SnowflakeDriver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+*
+* Create a connection to Snowflake for a given
+* - Account
+* - Private Key
+* - User
+*
+*/
 public class SnowflakeConnection {
 	@Getter @Setter private String account;
 	@Getter @Setter private String privateKey; 
 	@Getter @Setter private String user;
 	@Getter Connection conn;
 
+	/**
+	*
+	* Load the Snowflake driver class
+	*
+	*/
 	public SnowflakeConnection() {
 		try {
 			Class.forName("net.snowflake.client.jdbc.SnowflakeDriver");
@@ -25,23 +38,40 @@ public class SnowflakeConnection {
 		}
 	}
 
+	/**
+	*
+	* Close the connection to Snowflake
+	*
+	*/
 	public boolean closeConnection() {
 		conn.close();
 	}
 
+	/**
+	*
+	* Open the connection to Snowflake
+	*
+	*/
 	public boolean makeConnection() {
 		boolean rc = false;
+
+		// Build out the URL for the connection
 		StringBuffer url = new StringBuffer();
 		url = url.append("jdbc:snowflake://")
 			.append(account.trim())
 			.append(".snowflakecomputing.com");
+	
+		// Build the necessary properties
 		Properties prop = new Properties();
 		prop.put("user",user.trim());
 		prop.put("account",account.trim());
 		prop.put("password",privateKey.trim());
 
-		System.err.println(prop.toString());
-
+		/*
+		*
+		* Try the connection
+		*
+		*/
 		try {
 			conn = DriverManager.getConnection(url.toString(), prop);
 			System.err.println("Life is good when connected");
